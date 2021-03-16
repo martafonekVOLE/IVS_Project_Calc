62f40b09216f535736efef512e6e4022949e5d0e
kristofko <siskakuto@gmail.com>
2021-03-16 20:21:58 +0100

    Updated logarithm function

--- Math.js
+++ Math.js
@@ -1,41 +1,45 @@
-/* 
-    @author : Krištof Šiška
-    @licence : GNU General Public License v3
+/** 
+*  @author : Krištof Šiška
+*  @licence : GNU General Public License v3
 */
 
-/* JS MATH LIBRARY FOR IVS */
+/** JS MATH LIBRARY FOR IVS */
 
-/* @function Adds up 2 numbers 
-   @param {double} 1st number
-   @param {double} 2nd number
-   @return Returns sum of the 2 numbers 
+/** 
+ *  @function Adds up 2 numbers 
+ *  @param {double} 1st number
+ *  @param {double} 2nd number
+ *  @return Returns sum of the 2 numbers 
 */
 function addUp(a , b) {
     return a + b;
 }
 
-/* @function Subtracts 2nd number from the 1st
-   @param {double} Number to be subtracted from
-   @param {double} Number to be subtracted
-   @return Returns difference between num 1 and num 2 
+/**
+ *  @function Subtracts 2nd number from the 1st
+ *  @param {double} Number to be subtracted from
+ *  @param {double} Number to be subtracted
+ *  @return Returns difference between num 1 and num 2 
 */
 function subtract(a , b) {
     return a - b;
 }
 
-/* @function Multiplies the 2 numbres
-   @param {double} 1st number 
-   @param {double} 2nd number
-   @return Returns the multiplication value of the 2 numbers 
+/** 
+ *  @function Multiplies the 2 numbres
+ *  @param {double} 1st number 
+ *  @param {double} 2nd number
+ *  @return Returns the multiplication value of the 2 numbers 
 */
 function multiply(a , b){
     return a * b;
 }
 
-/* @function Divides 2 numbers
-   @param {double} divident
-   @param {double} divisor
-   @return Returns Quotient of the 2 numbers 
+/** 
+ * @function Divides 2 numbers
+ * @param {double} divident
+ * @param {double} divisor
+ * @return Returns Quotient of the 2 numbers 
 */
 function divide(a , b){
     if (b == 0){
@@ -46,9 +50,10 @@ function divide(a , b){
     return a/b;
 }
 
-/* @function Product of all integers <= a 
-   @param {int} Highest integer
-   @return Returns the factorial of a number 
+/** 
+ * @function Product of all integers <= a 
+ * @param {int} Highest integer
+ * @return Returns the factorial of a number 
 */
 function factorial(a){
     if (a < 0){
@@ -64,36 +69,38 @@ function factorial(a){
     return res;
 }
 
-/* @function Repeated multiplication of a base to the power of exponent 
-   @param {double} Base
-   @param {double} Exponent
-   @return Returns the 1st num raised to the power of the 2nd num 
+/** 
+ *  @function Repeated multiplication of a base to the power of exponent 
+ *  @param {double} Base
+ *  @param {double} Exponent
+ *  @return Returns the 1st num raised to the power of the 2nd num 
 */
 function toPower(a , b){
     return Math.toPower(a , b);
 }
 
-/* @function Function to return the square root of a number
-   @param {double}
-   @return {double} Returns  the square root of a given number  
+/** 
+ *  @function Function to return the square root of a number
+ *  @param {double}
+ *  @return {double} Returns  the square root of a given number  
  */
 function squareRoot(a){
     return Math.sqrt(a);
 }
 
-/* @function Function to return logarithm to the base of 10
-   @param {double}
-   @return {double} Returns the logarithm to the base of 10 of a given number 
+/** 
+ *  @function Function to return logarithm to the base of 10
+ *  @param {double}
+ *  @return {double} Returns the logarithm to the base of 10 of a given number 
 */
 function logarithm(a){
-    return Math.log10(a);
+    return Math.log(a);
 }
 
-
-
-/* @function Function to return absolute value of a number
-   @param {double}
-   @return {double} Returns the absolute value of a number 
+/** 
+ *  @function Function to return absolute value of a number
+ *  @param {double}
+ *  @return {double} Returns the absolute value of a number 
 */
 function abs(a){
     return Math.abs(a);
