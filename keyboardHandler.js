a02a4177d0716c7e76171d42bd3997ad7d21b1b5
martafonekVOLE <martafonek.1@gmail.com>
2021-03-26 12:11:09 +0100

    Preparation for main (master) branch push
    Everything was successfully linked together, all is working together as
    it should.
    Push into main branch is ready with working version 1.0.0

--- keyboardHandler.js
+++ keyboardHandler.js
@@ -8,6 +8,7 @@ function insertNumber(number){
     }
     document.getElementById('inputField').innerHTML = document.getElementById('inputField').innerHTML + number;
 }
+/*Backspace function*/
 function backspace(){
     var backspace = document.getElementById('inputField').innerHTML;
     var backspace = backspace.slice(0, -1);
@@ -23,55 +24,135 @@ var NumbersAndOperation = {};
 /***Identifies Operator and printf onto intermediate Display*/
 function operation(operator){
     NumbersAndOperation.operator = operator;
-    var test = document.getElementById('inputField').innerHTML;
+    var test = parseFloat(document.getElementById('inputField').innerHTML);     //parse
     var firstNumber;
     if(test==""){
-        alert('NULL');
+        alert('NULL');      //Possible -+ change
     }
     else{
-        NumbersAndOperation.firstNumber = document.getElementById('inputField').innerHTML;
+        NumbersAndOperation.firstNumber = parseFloat(document.getElementById('inputField').innerHTML);
 
-    document.getElementById('intermediate').innerHTML = document.getElementById('inputField').innerHTML;
+    document.getElementById('intermediate').innerHTML = NumbersAndOperation.firstNumber;
     document.getElementById('inputField').innerHTML = '';
     }
     if(operator==1){
         document.getElementById('intermediate').innerHTML = document.getElementById('intermediate').innerHTML + '+';
+    }
+    else if (operator==2){
+        document.getElementById('intermediate').innerHTML = document.getElementById('intermediate').innerHTML + '-';
+    }
+    else if (operator==3){
+        document.getElementById('intermediate').innerHTML = document.getElementById('intermediate').innerHTML + '*';
+    }
+    else if (operator==4){
+        document.getElementById('intermediate').innerHTML = document.getElementById('intermediate').innerHTML + '÷';
+    }
+    else if (operator==5){
+        document.getElementById('intermediate').innerHTML = document.getElementById('intermediate').innerHTML + '^';
+    }
+    else if (operator==6){
+        document.getElementById('intermediate').innerHTML ='√' + document.getElementById('intermediate').innerHTML;
+    }
+    else if (operator==7){
+        document.getElementById('intermediate').innerHTML ='log' + document.getElementById('intermediate').innerHTML;
+    }
+    else if (operator==8){
+        document.getElementById('intermediate').innerHTML ='|' + document.getElementById('intermediate').innerHTML + "|";
+    }
+    else if (operator==9){
+        document.getElementById('intermediate').innerHTML = document.getElementById('intermediate').innerHTML + "!";
     }
 
 }
 /*RESULT OF EPERATION*/
 function result(){
     var firstNumber = NumbersAndOperation.firstNumber;
-    var secondNumber = document.getElementById('inputField').innerHTML;;
+    var secondNumber = parseFloat(document.getElementById('inputField').innerHTML);
+    if(NumbersAndOperation.operator<6){
     document.getElementById('intermediate').innerHTML = document.getElementById('intermediate').innerHTML + secondNumber;
+    }
+
     if(NumbersAndOperation.operator==1){
         /*Sčítání*/
-        var resultOfOperation = (+secondNumber + +firstNumber);
+        resultOfOperation = addUp(firstNumber, secondNumber);
         document.getElementById('inputField').innerHTML = resultOfOperation;
         NumbersAndOperation.calculated = 1;
         NumbersAndOperation.Ans = resultOfOperation;
         NumbersAndOperation.Ans1 = firstNumber;
         NumbersAndOperation.Ans2 = secondNumber;
-        /*
-        If you make operation with converted number, such as "-21+3" you get the right result, but without "-" symbol. Fix with conditions?
-        */
     }
-    if(NumbersAndOperation.operator==2){
+    else if(NumbersAndOperation.operator==2){
         /*Odčítání*/
+        resultOfOperation = subtract(firstNumber, secondNumber);
+        document.getElementById('inputField').innerHTML = resultOfOperation;
+        NumbersAndOperation.calculated = 1;
+        NumbersAndOperation.Ans = resultOfOperation;
+        NumbersAndOperation.Ans1 = firstNumber;
+        NumbersAndOperation.Ans2 = secondNumber;
     }
-    if(NumbersAndOperation.operator==3){
+    else if(NumbersAndOperation.operator==3){
         /*Násobení*/
+        resultOfOperation = multiply(firstNumber, secondNumber);
+        document.getElementById('inputField').innerHTML = resultOfOperation;
+        NumbersAndOperation.calculated = 1;
+        NumbersAndOperation.Ans = resultOfOperation;
+        NumbersAndOperation.Ans1 = firstNumber;
+        NumbersAndOperation.Ans2 = secondNumber;
     }
-    if(NumbersAndOperation.operator==4){
+    else if(NumbersAndOperation.operator==4){
         /*Dělení*/
+        resultOfOperation = divide(firstNumber, secondNumber);
+        document.getElementById('inputField').innerHTML = resultOfOperation;
+        NumbersAndOperation.calculated = 1;
+        NumbersAndOperation.Ans = resultOfOperation;
+        NumbersAndOperation.Ans1 = firstNumber;
+        NumbersAndOperation.Ans2 = secondNumber;
     }
+    else if(NumbersAndOperation.operator==5){           //DOES NOT WORK 
+        resultOfOperation = toPower(firstNumber, secondNumber);
+        document.getElementById('inputField').innerHTML = resultOfOperation;
+        NumbersAndOperation.calculated = 1;
+        NumbersAndOperation.Ans = resultOfOperation;
+        NumbersAndOperation.Ans1 = firstNumber;
+        NumbersAndOperation.Ans2 = secondNumber;
+    }
+    else if(NumbersAndOperation.operator==6){
+        resultOfOperation = squareRoot(firstNumber);
+        document.getElementById('inputField').innerHTML = resultOfOperation;
+        NumbersAndOperation.calculated = 1;
+        NumbersAndOperation.Ans = resultOfOperation;
+        NumbersAndOperation.Ans1 = firstNumber;
+    }
+    else if(NumbersAndOperation.operator==7){
+        resultOfOperation = logarithm(firstNumber);
+        document.getElementById('inputField').innerHTML = resultOfOperation;
+        NumbersAndOperation.calculated = 1;
+        NumbersAndOperation.Ans = resultOfOperation;
+        NumbersAndOperation.Ans1 = firstNumber;
+    }
+    else if(NumbersAndOperation.operator==8){
+        resultOfOperation = abs(firstNumber);
+        document.getElementById('inputField').innerHTML = resultOfOperation;
+        NumbersAndOperation.calculated = 1;
+        NumbersAndOperation.Ans = resultOfOperation;
+        NumbersAndOperation.Ans1 = firstNumber;
+    }
+    else if(NumbersAndOperation.operator==9){
+        resultOfOperation = factorial(firstNumber);
+        document.getElementById('inputField').innerHTML = resultOfOperation;
+        NumbersAndOperation.calculated = 1;
+        NumbersAndOperation.Ans = resultOfOperation;
+        NumbersAndOperation.Ans1 = firstNumber;
+    }
+
 }
 /*PREVIOUS RESULT*/
 function getAns(){
     NumbersAndOperation.calculated = 1;
     document.getElementById('inputField').innerHTML = NumbersAndOperation.Ans;
     if(NumbersAndOperation.operator == 1){
-    document.getElementById('intermediate').innerHTML = NumbersAndOperation.Ans1 + "+" +NumbersAndOperation.Ans2+ "=" + NumbersAndOperation.Ans;
+    document.getElementById('intermediate').innerHTML = NumbersAndOperation.Ans;
+    NumbersAndOperation.firstNumber=NumbersAndOperation.Ans;
     }
 }
 /*CONVERT +-*/
